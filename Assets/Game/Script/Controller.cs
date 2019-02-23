6aaa847568081780403565ef04319b6113d58c51
Adrian Ng <adrianng909@gmail.com>
2019-02-23 22:11:04 +0000

    bullet now shoots

--- Assets/Game/Script/Controller.cs
+++ Assets/Game/Script/Controller.cs
@@ -107,9 +107,6 @@ public class Controller : MonoBehaviour
         // Switch the way the player is labelled as facing.
         m_FacingRight = !m_FacingRight;
 
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
+        transform.Rotate(0f, 180f, 0f);
     }
 }
---
+++ Assets/Game/Script/Weapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject arrow;
+    public float time;
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
+            if (time >= 2)
+            {
+                time -= Time.deltaTime;
+            }
+            else
+            {
+                //Do Something after clock hits 0
+                Shoot();
+                time = time * time;
+            }
+        
+        //Do Something else while clock counting down
+    }
+
+    void Shoot()
+    {
+        Instantiate(arrow, firePoint.position, firePoint.rotation);
+    }
+}
