d3531b8c709ba3032c7a93da3d6bb7e1dbb171d6
HannAce <h.fisher5@uni.brighton.ac.uk>
2019-02-23 17:48:45 +0000

    Menu system finished

---
+++ Assets/MainMenu/Scripts/LM.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LM : MonoBehaviour {
+
+    public static LM singleton;
+
+	// Use this for initialization
+	void Awake () {
+        if (singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else if (singleton != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public static void ReloadLevel(int vLevel)
+    {
+        SceneManager.LoadScene(vLevel);
+    }
+}
